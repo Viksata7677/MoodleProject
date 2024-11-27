a997d50d2bdfa6e5f114f75676a6e946bb9934f0
Viktor Iordanov <viki1204394@gmail.com>
2024-11-27 14:21:21 +0200

    Fix: Teacher won't be able to access homework upload, edit and delete url

--- MoodleProject/homeworks/views.py
+++ MoodleProject/homeworks/views.py
@@ -4,6 +4,7 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accounts.models import Student
 from homeworks.forms import HomeworkCreateForm, HomeworkEditForm, HomeworkDeleteForm, HomeworkGradeForm
+from homeworks.mixins import PermissionRequiredMixin
 from homeworks.models import Homework
 
 
@@ -15,17 +16,16 @@ from homeworks.models import Homework
 #       return render(request, '404.html', status=404)
 
 
-class HomeworkUploadPage(LoginRequiredMixin, CreateView):
+class HomeworkUploadPage(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Homework
     form_class = HomeworkCreateForm
     template_name = 'homework/homework-upload.html'
     success_url = reverse_lazy('dashboard')
+    permission_required = 'homeworks.add_homework'
+    permission_denied_message = "Can't upload homework as a teacher"
 
     def form_valid(self, form):
-        homework = form.save(commit=False)
-
-        homework.student = self.request.user.student_profile
-
+        form.instance.student = self.request.user.student_profile
         return super().form_valid(form)
 
 
@@ -35,20 +35,24 @@ class HomeworkDetailPage(LoginRequiredMixin, DetailView):
     context_object_name = 'homework'
 
 
-class HomeworkEditPage(LoginRequiredMixin, UpdateView):
+class HomeworkEditPage(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Homework
     form_class = HomeworkEditForm
     template_name = 'homework/homework-edit.html'
+    permission_required = 'homeworks.change_homework'
+    permission_denied_message = "Can't edit if you are not a student."
 
     def get_success_url(self):
         return reverse_lazy('homework-details', kwargs={'pk': self.object.pk})
 
 
-class HomeworkDeletePage(DeleteView):
+class HomeworkDeletePage(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Homework
     form_class = HomeworkDeleteForm
     template_name = 'homework/homework-delete.html'
     success_url = reverse_lazy('dashboard')
+    permission_required = 'homeworks.delete_homework'
+    permission_denied_message = "Can't delete if you are not a student."
 
     def get_initial(self) -> dict:
         return self.get_object().__dict__
