071f6e3ff05dd134f5d0ce9ea5e2c206a6277350
Viktor Iordanov <viki1204394@gmail.com>
2024-12-07 19:58:42 +0200

    Add: Swagger

--- MoodleProject/settings.py
+++ MoodleProject/settings.py
@@ -45,7 +45,19 @@ INSTALLED_APPS = [
     "tests.apps.TestsConfig",
 
     'rest_framework',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'MoodleProject',
+    'DESCRIPTION': 'MoodleProject',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- MoodleProject/urls.py
+++ MoodleProject/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 from MoodleProject import settings
 
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('homeworks/', include('homeworks.urls')),
